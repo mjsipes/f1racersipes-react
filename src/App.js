c4652bb967e59b464c6e3278576b27bb392e1408
Michael Sipes <msipes@usc.edu>
2025-04-30 16:16:48 -0700

    <SpeedInsights />

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Pregaming from "./pages/pregaming";
 import StartGame from "./pages/startgame";
 import JoinGame from "./pages/joingame";
 import Game from "./pages/game";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 function App() {
   return (
     <div className="App">
@@ -20,6 +21,7 @@ function App() {
         <Route path="/joingame" element={<JoinGame />} />
         <Route path="/game/*" element={<Game />} />
       </Routes>
+      <SpeedInsights />
     </div>
   );
 }
